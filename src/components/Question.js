8db17bf8c428b7f0fde67d70f58ec17eaee1e95e
TONNYCHWEYA <chweyatonny@gmail.com>
2022-07-17 10:42:16 +0300

    commit tree 132869b28588c0d63621e228e1e878369ece6542

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+  useEffect(() =>{
+    const idTimer = setTimeout(() =>{setTimeRemaining((timeRemaining) => timeRemaining - 1)},1000)
+    if(timeRemaining === 0) {
+      onAnswered(false)
+      return setTimeRemaining(10)
+     
+    }
+    return function cleanup(){
+      clearTimeout(idTimer)
+    }
+
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
